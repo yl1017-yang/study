a44b1656d08f8ea42319d752555a2d278d6a3f99
yu yang lae <yl.yu@pulmuone.com>
2024-03-14 14:02:29 +0900

    inex

--- pulmuone/vue.config.js
+++ pulmuone/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir:'./docs',
-  // publicPath:'/resources/study/pulmuone',
+  publicPath:'/pulmuone/',
   // publicPath: process.env.NODE_ENV === 'resources'
   //   ? '/study/'
   //   : '/pulmuone'
