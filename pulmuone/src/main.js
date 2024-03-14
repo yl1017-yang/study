3ace3fc8487b457ce43dbe78251bf351f2fd45bf
yu yang lae <yl.yu@pulmuone.com>
2024-03-14 17:37:55 +0900

    bulld

--- pulmuone/src/main.js
+++ pulmuone/src/main.js
@@ -2,10 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// const router = createRouter({
-//   history: createWebHistory('/pulmuone/'),
-//   routes
-// })
-
-// createApp(App).mount('#app')
 createApp(App).use(router).mount('#app') 
